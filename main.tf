81abeafc022054541c15b6a73e92e2dcd8be87e1
yunfengsg <yunfengsg@gmail.com>
2025-02-15 11:55:25 +0800

    feature push03

--- main.tf
+++ main.tf
@@ -3,11 +3,23 @@ provider "aws" {
 }
 
 terraform {
-    required_version = ">= 1.0" 
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+locals {
+  prefix = "yyf"  # Replace with your desired prefix
 }
 
 data "aws_caller_identity" "current" {}
 
+data "aws_region" "current" {}
 
 resource "aws_ecr_repository" "ecr" {
   name         = "${local.prefix}-ecr"
@@ -28,25 +40,26 @@ module "ecs" {
   }
 
   services = {
-    YOUR-TASKDEFINITION-NAME = { yyf-coaching17-task
+    yyf-coaching17-task = {
       cpu    = 512
       memory = 1024
-      container_definitions = {
-        YOUR-CONTAINER-NAME = { yyf-coaching17-container
+      container_definitions = jsonencode([
+        {
+          name      = "yyf-coaching17-container"
           essential = true
           image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"
-          port_mappings = [
+          portMappings = [
             {
               containerPort = 8080
               protocol      = "tcp"
             }
           ]
         }
-      }
+      ])
       assign_public_ip                   = true
       deployment_minimum_healthy_percent = 100
-      subnet_ids                   = [subnet-0bbee1ca446e01642] #List of subnet IDs to use for your tasks
-      security_group_ids           = [sg-09662e0e5a3857a26] #Create a SG resource and pass it here
+      subnet_ids                         = ["subnet-0bbee1ca446e01642"] # Use quotes for strings
+      security_group_ids                 = ["sg-09662e0e5a3857a26"]    # Use quotes for strings
     }
   }
 }
