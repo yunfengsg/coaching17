a275a3e5302ae7cf44223dc865416ade850b0422
yunfengsg <yunfengsg@gmail.com>
2025-02-16 16:50:12 +0800

    feature push 10

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Use a valid version constraint
+    }
+  }
+}
+
 provider "aws" {
-    region = "ap-southeast-1"
+  region = "ap-southeast-1"
 }
