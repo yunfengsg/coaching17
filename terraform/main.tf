d3360bcce06ec80f755cf2d02e80c03dace40568
yunfengsg <yunfengsg@gmail.com>
2025-02-16 17:16:21 +0800

    Update AWS provider version

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ module "ecs" {
       memory = 1024
       # Container definition(s)
       container_definitions = {
-        jaz-ecs-container = { #container name -> Change
+        yyf-ecs-container = { #container name -> Change
           essential = true
           image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"
           port_mappings = [
